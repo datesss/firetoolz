caadee895f27f71547b3e521382178b12c76f0ad
datesss <andreadytofall@gmail.com>
2016-12-09 00:26:09 -0500

    tracking pizzagate

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// import TwitterBot from 'node-twitterbot';
+// const Bot = new TwitterBot.TwitterBot(secrets);
+import Twit from 'twit';
+import secrets from './secrets';
+
+const T = new Twit(secrets);
+
+const stream = T.stream('statuses/filter', { track: 'pizzagate' });
+
+stream.start();
+stream.on('tweet', (tweet) => {
+  console.log(tweet.text);
+});
